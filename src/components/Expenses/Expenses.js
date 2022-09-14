29a73901e40d9a15598cf8e195657cf4ce4576f7
Kevin Leister <kevinleister07@gmail.com>
2022-09-14 18:29:02 -0500

    initial commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
+
+function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('Show All');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.items.filter((expense) => {
+        if (filteredYear !== 'Show All') {
+            return expense.date.getFullYear().toString() === filteredYear;
+        } else {
+            return true;
+        }
+    });
+
+    return (
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
+                />
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
+            </Card>
+        </div>
+    );
+}
+
+export default Expenses;
